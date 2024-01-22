81712e31260163e1be523858fc6cd1b5af7a2e9f
Lenard <le.wild@mci4me.at>
2024-01-22 10:10:56 +0100

    Updated User class to include role and added update and delete methods

--- users.py
+++ users.py
@@ -1,13 +1,29 @@
+from tinydb import TinyDB, Query
+
 class User:
-    def __init__(self, id, name) -> None:
+    def __init__(self, id, name, role) -> None:
         self.id = id
         self.name = name
+        self.role = role
+        self.db = TinyDB('db.json')  # replace 'db.json' with the path to your database file
 
     def store_data(self):
-        # Implement this method to store user data in the database
-        pass
+        self.db.insert({'id': self.id, 'name': self.name, 'role': self.role})
+
+    def update_data(self, name=None, role=None):
+        User = Query()
+        updated_data = {}
+        if name is not None:
+            updated_data['name'] = name
+        if role is not None:
+            updated_data['role'] = role
+        self.db.update(updated_data, User.id == self.id)
+
+    def delete_data(self):
+        User = Query()
+        self.db.remove(User.id == self.id)
 
     @staticmethod
     def load_data_by_user_id(id):
-        # Implement this method to load user data from the database
-        pass
+        User = Query()
+        return db.search(User.id == id)
