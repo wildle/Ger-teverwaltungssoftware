a3fcad569a9d900ec16c4a7f9e114bdb11b9de0b
Lenard <le.wild@mci4me.at>
2024-01-29 21:19:02 +0100

    Refactor queries.py to use a QueryDB class for finding devices and users

--- queries.py
+++ queries.py
@@ -2,20 +2,33 @@ import os
 from tinydb import TinyDB, Query
 from serializer import serializer
 
-def find_devices() -> list:
-    """Find all devices in the database."""
-    # Define the database connector
-    db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('devices')
-    # Create a query object
-    DeviceQuery = Query()
-    # Search the database for all devices that are active
-    result = db_connector.all()
-    
-    # The result is a list of dictionaries, we only want the device names
-    if result:
-        result = [x["device_name"] for x in result]
-    
-    return result
+class QueryDB:
+    db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer)
+
+    @classmethod
+    def find_devices(cls) -> list:
+        """Find all devices in the database."""
+        devices_table = cls.db_connector.table('devices')
+        DeviceQuery = Query()
+        result = devices_table.all()
+
+        if result:
+            result = [x["device_name"] for x in result]
+
+        return result
+
+    @classmethod
+    def find_users(cls) -> list:
+        """Find all users in the database."""
+        users_table = cls.db_connector.table('users')
+        UserQuery = Query()
+        result = users_table.all()
+
+        if result:
+            result = [x["username"] for x in result]
+
+        return result
 
 if __name__ == "__main__":
-    print(find_devices())
+    print(QueryDB.find_devices())
+    print(QueryDB.find_users())
